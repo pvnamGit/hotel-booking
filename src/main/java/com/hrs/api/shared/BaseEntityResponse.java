ed1c30481cec43b60f6706c371ebac728089f814
pvnamGit <pvnam.forwork@gmail.com>
2024-06-08 00:09:44 +0700

    feat: make base response implement serializable

--- src/main/java/com/hrs/api/shared/BaseEntityResponse.java
+++ src/main/java/com/hrs/api/shared/BaseEntityResponse.java
@@ -1,6 +1,7 @@
 package com.hrs.api.shared;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import java.io.Serializable;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,37 +10,25 @@ import lombok.Data;
 @Builder
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class BaseEntityResponse<T> {
-    private Boolean success;
-    private String message;
-    private BaseErrorResponse error;
-    private T data;
+public class BaseEntityResponse<T> implements Serializable {
+  private Boolean success;
+  private String message;
+  private BaseErrorResponse error;
+  private T data;
 
-    public static <T> BaseEntityResponse<T> success() {
-        return BaseEntityResponse.<T>builder()
-                .success(Boolean.TRUE)
-                .build();
-    }
+  public static <T> BaseEntityResponse<T> success() {
+    return BaseEntityResponse.<T>builder().success(Boolean.TRUE).build();
+  }
 
-    public static <T> BaseEntityResponse<T> success(String message) {
-        return BaseEntityResponse.<T>builder()
-                .success(Boolean.TRUE)
-                .message(message)
-                .build();
-    }
+  public static <T> BaseEntityResponse<T> success(String message) {
+    return BaseEntityResponse.<T>builder().success(Boolean.TRUE).message(message).build();
+  }
 
-    public static <T> BaseEntityResponse<T> success(T data) {
-        return BaseEntityResponse.<T>builder()
-                .success(Boolean.TRUE)
-                .message(null)
-                .data(data)
-                .build();
-    }
+  public static <T> BaseEntityResponse<T> success(T data) {
+    return BaseEntityResponse.<T>builder().success(Boolean.TRUE).message(null).data(data).build();
+  }
 
-    public static <T> BaseEntityResponse<T> error(BaseErrorResponse error) {
-        return BaseEntityResponse.<T>builder()
-                .success(Boolean.FALSE)
-                .error(error)
-                .build();
-    }
+  public static <T> BaseEntityResponse<T> error(BaseErrorResponse error) {
+    return BaseEntityResponse.<T>builder().success(Boolean.FALSE).error(error).build();
+  }
 }
--- src/main/java/com/hrs/api/shared/BasePaginationResponse.java
+++ src/main/java/com/hrs/api/shared/BasePaginationResponse.java
@@ -1,6 +1,7 @@
 package com.hrs.api.shared;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import java.io.Serializable;
 import java.util.List;
 import lombok.*;
 
@@ -8,31 +9,25 @@ import lombok.*;
 @Builder
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class BasePaginationResponse<T> {
-    private Boolean success;
-    private String message;
-    private Integer total;
-    private List<T> data;
+public class BasePaginationResponse<T> implements Serializable {
+  private Boolean success;
+  private String message;
+  private Integer total;
+  private List<T> data;
 
-    public static<T> BasePaginationResponse success(List<T> data, Integer total) {
-        return BasePaginationResponse.<T>builder()
-                .success(Boolean.TRUE)
-                .total(total)
-                .data(data)
-                .build();
-    }
+  public static <T> BasePaginationResponse success(List<T> data, Integer total) {
+    return BasePaginationResponse.<T>builder()
+        .success(Boolean.TRUE)
+        .total(total)
+        .data(data)
+        .build();
+  }
 
-    public static<T> BasePaginationResponse success(List<T> data) {
-        return BasePaginationResponse.<T>builder()
-                .success(Boolean.TRUE)
-                .data(data)
-                .build();
-    }
+  public static <T> BasePaginationResponse success(List<T> data) {
+    return BasePaginationResponse.<T>builder().success(Boolean.TRUE).data(data).build();
+  }
 
-    public static <T> BasePaginationResponse<T> error(String message) {
-        return BasePaginationResponse.<T>builder()
-                .success(Boolean.FALSE)
-                .message(message)
-                .build();
-    }
+  public static <T> BasePaginationResponse<T> error(String message) {
+    return BasePaginationResponse.<T>builder().success(Boolean.FALSE).message(message).build();
+  }
 }
